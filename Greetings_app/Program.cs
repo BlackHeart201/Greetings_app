3c26e094bb95758ceddf1c2411dba7161794618c
Piotrek <polak.piotr94@gmail.com>
2022-10-28 05:07:04 +0200

    Dodaj pliki projektów.

---
+++ Greetings_app/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Greetings
+{
+    class Welcome
+    {
+        static void Main(string[] args) 
+        {
+            string name = "" ;
+
+            string nameQuestion = "What's your name?";
+
+            string welcomeGreetings = "Hello ";
+
+            Console.WriteLine(nameQuestion);
+
+            name = Console.ReadLine();
+
+            Console.WriteLine(welcomeGreetings + name);
+
+        }
+    }
+
+}
