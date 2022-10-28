198db596e2edc3eea15bbbd86d97c659ec875b8e
Piotrek <polak.piotr94@gmail.com>
2022-10-28 05:24:15 +0200

    Basics improvement

--- Greetings_app/Program.cs
+++ Greetings_app/Program.cs
@@ -6,18 +6,36 @@ namespace Greetings
     {
         static void Main(string[] args) 
         {
-            string name = "" ;
+            string name;
+            int age;
+            char sex;
 
-            string nameQuestion = "What's your name?";
+            string generalQuestion = "What's your ";
+            string nameQuestion = generalQuestion + "name?";
+            string ageQuestion = generalQuestion + "age?";
+            string sexQuestion = generalQuestion + "sex?";
 
-            string welcomeGreetings = "Hello ";
+            string femaleGreetings = "Hello Ms. ";
+            string maleGreetings = "Hello Mrs. ";
+
+            string greetingsEnding = " years old. Nice to meet you!";
 
             Console.WriteLine(nameQuestion);
 
             name = Console.ReadLine();
 
-            Console.WriteLine(welcomeGreetings + name);
+            Console.WriteLine(ageQuestion);
 
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(sexQuestion);
+
+            sex = Convert.ToChar(Console.ReadLine());
+
+            if (sex == 'M')
+                Console.WriteLine(maleGreetings + name + ", " + age + greetingsEnding);
+            else
+                Console.WriteLine(femaleGreetings + name + ", " + age + greetingsEnding);
         }
     }
 
