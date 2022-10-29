34e8bf72d1ea3b13266480f9520de2b94f2e3ebe
Piotrek <polak.piotr94@gmail.com>
2022-10-29 16:50:28 +0200

    Added format checker

--- Greetings_app/Program.cs
+++ Greetings_app/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 namespace Greetings
 {
@@ -6,36 +7,41 @@ namespace Greetings
     {
         static void Main(string[] args) 
         {
-            string name;
-            int age;
-            char sex;
-
             string generalQuestion = "What's your ";
             string nameQuestion = generalQuestion + "name?";
             string ageQuestion = generalQuestion + "age?";
             string sexQuestion = generalQuestion + "sex?";
+            string greetingsEnding = " years old. Nice to meet you!";
 
             string femaleGreetings = "Hello Ms. ";
             string maleGreetings = "Hello Mrs. ";
 
-            string greetingsEnding = " years old. Nice to meet you!";
+            try
+            {
+                Console.WriteLine(nameQuestion);
+                string name = Console.ReadLine();
 
-            Console.WriteLine(nameQuestion);
+                Console.WriteLine(ageQuestion);
+                int age = Convert.ToInt32(Console.ReadLine());
 
-            name = Console.ReadLine();
+                Console.WriteLine(sexQuestion);
+                char sex = Convert.ToChar(Console.ReadLine());
 
-            Console.WriteLine(ageQuestion);
+                if (sex == 'M' || sex == 'm')
+                    Console.WriteLine(maleGreetings + name + ", " + age + greetingsEnding);
 
-            age = Convert.ToInt32(Console.ReadLine());
+                else if (sex == 'F' || sex == 'f')
+                    Console.WriteLine(femaleGreetings + name + ", " + age + greetingsEnding);
 
-            Console.WriteLine(sexQuestion);
+                else
+                    Console.WriteLine("Given sex has incorect value");
 
-            sex = Convert.ToChar(Console.ReadLine());
+            }
 
-            if (sex == 'M')
-                Console.WriteLine(maleGreetings + name + ", " + age + greetingsEnding);
-            else
-                Console.WriteLine(femaleGreetings + name + ", " + age + greetingsEnding);
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 
